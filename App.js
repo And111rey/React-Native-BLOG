ee2ac1a1f606e8b83c8e2268fda20cc784a1ad8b
Andrey <samu111ray@gmail.com>
2019-12-26 22:31:23 +0200

    RESTART

--- App.js
+++ App.js
@@ -1,22 +1,21 @@
-import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { AppLoading } from "expo"
-import { bootstrap } from './src/bootstrap'; //модуль со шрифтами
+import React from  "react"
 
-import { AppNavigator } from "./src/navigation/AppNavigation"
 
-export default function App() {
-
-  const [isReady, setIsReady] = useState(false)
-
-  if (!isReady) {
-    return < AppLoading 
-      startAsync={bootstrap}    //передаем шрифты
-      onFinish={() => {setIsReady(true)}}
-      onError={err => console.log(err)} 
-    />
-  } 
-  
-  return < AppNavigator /> // оболочка со всеми скринами
+export  default function App() {
+  return (
+    <View style={styles.conteiner}>
+      <Text>Hello</Text>
+    </View>
+  )
 }
 
+
+const styles = StyleSheet.create({
+  conteiner: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: 'center' 
+  }
+})
--- src/theme.js
+++
@@ -1,7 +0,0 @@
-
-
-
-export const THEME = {
-    MAIN_COLOR: "#303f9f",
-    DANGER_COLOR: "#d81b60"
-}
